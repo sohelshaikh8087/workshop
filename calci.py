8e053b7fa5c8db0c763bc28172721aa35e7d26be
sohel shaikh <shaikhsohel9637@gmail.com>
2018-07-15 15:56:55 +0530

    My first module
    Signed-off-by: sohel shaikh <shaikhsohel9637@gmail.com>

---
+++ calci.py
@@ -0,0 +1,9 @@
+class calci():
+    def add(a,b):
+        print("addition",a+b)
+    def sub(a,b):
+        print("substraction",a-b)
+    def mul(a,b):
+        print("multiplication",a*b)
+    def div(a,b):
+        print("division",a/b)
